49228dbd3e1d3b35b645955e7e452467ca3d1840
Mohammed Faisal Khan <faisalkhan4k@gmail.com>
2024-03-20 07:05:42 +0530

    Add Functionalities to the Operator Buttons
    Perform basic Mathematical operations such as '-', '*', '^', '/'
    Update the Calculator logic on when to give output.

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
-
 const heading = document.querySelector('h1');
 const input = document.querySelector('.input')
 
 
+let num1='', num2='' , operatorEntered='';
 
 
 
@@ -46,8 +45,13 @@ const numbersbuttons = Array.from(numberedButtons);
 console.log(numbersbuttons);
 
 //Populating the Input div with numbers
+
+
 numbersbuttons.forEach((button)=>{
     button.addEventListener('click',()=>{
+
+        num1+=button.textContent;
+
         input.textContent+=button.textContent;
 
     });
@@ -61,26 +65,41 @@ console.log(operatorsbuttons);
 
 operatorsbuttons.forEach((operator)=>{
     operator.addEventListener('click',()=>{
-        
-        input.textContent+=operator.textContent;
+        if(operatorEntered!=''){
+            
+            num1 = (arthematicOperations(operatorEntered,num2,num1));
+            operatorEntered='';
+        }
+        num2=num1;
+        num1='';
+        operatorEntered = operator.textContent;
+        console.log(operatorEntered);
+        if(num2==''){
+            alert('enter a number first');
+            operatorEntered='';
+        }
+        else input.textContent+=operator.textContent;
         
         
     })
 })
 
+const equalsTo =  document.querySelector('.longbuttons').appendChild(document.createElement('button'));
+equalsTo.textContent = "="
+equalsTo.addEventListener('click',()=>{
+   num1 = (arthematicOperations(operatorEntered,num2,num1));
+   operatorEntered='';
+})
+
+
 
 function arthematicOperations(operator , num1,num2){
-    if(operator == '+') input.textContent = num1+num2;
-    else if(operator == '-') input.textContent = num1-num2;
-    else if(operator == '*') input.textContent = num1*num2;
-    else if(operator == '/') input.textContent = num1/num2;
-    else if(operator == '^') input.textContent = math.pow(num1,num2);
+    if(operator == '+') return input.textContent = num1+num2;
+    else if(operator == '-') return input.textContent = num1-num2;
+    else if(operator == '*') return input.textContent = num1*num2;
+    else if(operator == '/') return input.textContent = num1/num2;
+    else if(operator == '^') return input.textContent = Math.pow(num1,num2);
 }
-
-
-
-
-
 
 //Entering another operator or clicking equals to sign gives the result
 
